6cf200147a58ca43e7ff1c41858363f7cc64264e
nayan kumar <nayans808@gmail.com>
2025-01-09 00:08:18 +0530

    initialised the frontend

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,11 @@
+export const Quote = () => {
+
+return(<div className="bg-slate-800">
+    <h1 className="text-8xl font-bold underline ">
+      Hello world!
+    </h1>
+    </div>
+)
+
+    
+};
---
+++ frontend/src/pages/Signup.tsx
@@ -0,0 +1,11 @@
+import { Quote } from "../components/Quote";
+
+export const Signup = () => {
+  return (
+    <>
+      <div  className="bg-slate-800">
+        <Quote />
+      </div>
+    </>
+  );
+};
